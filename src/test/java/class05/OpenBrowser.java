f6d711b8bf679833e025b5c688314389607964c6
sergeystrik <sergeystrik@gmail.com>
2024-05-08 21:56:24 -0400

    1commit

---
+++ src/test/java/class05/OpenBrowser.java
@@ -0,0 +1,13 @@
+package class05;
+
+import utils.CommonMethods;
+
+import java.io.IOException;
+
+public class OpenBrowser extends CommonMethods {
+    public static void main(String[] args) throws IOException {
+
+        openBrowserAndLaunchApplication();
+
+    }
+}
---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Constants {
+    public static final String CONFIG_FILE_PATH= System.getProperty("user.dir")+"/src/test/resources/config/config.properties";
+
+}
+
+
