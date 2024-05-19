f42ec488d77de1334599b1570480ab000c537dfb
sergeystrik <sergeystrik@gmail.com>
2024-05-19 10:53:03 -0400

    Selenium

---
+++ src/test/java/review03/tables.java
@@ -0,0 +1,24 @@
+package review03;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class tables {
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+//
+        driver.manage().window().maximize();
+//        goto hrms
+        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+//        login
+        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+
+        driver.findElement(By.id("txtPassword")).sendKeys("Hum@nhrm123");
+
+        driver.findElement(By.id("btnLogin")).click();
+
+//click on PIm option
+        driver.findElement(By.id("menu_pim_viewPimModule")).click();
+    }
+}
